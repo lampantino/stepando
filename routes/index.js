f583b9df976e27fdb59de79f6f8fc842b671dc8c
lampantino <sre.quereck@gmail.com>
2014-10-10 10:44:03 +0200

    Added an user edit form

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ var users = require('mongoose').model('users');
 //renderizar el archivo index.jade que está dentro de la carpeta
 //views. Se le pasa como argumento un objeto json.
 router.get('/', function(req, res) {
-  res.render('index', { title: 'stepando' });
+  res.render(
+      'index', 
+      { title: 'stepando' }
+  );
 });
 
 //Añadimos una página de alta
@@ -29,54 +32,100 @@ router.post('/addUser', function(req, res) {
     });
     
     //Almacenamos el nuevo usuario
-    addUser.save(function(err, addUser) {
-        if (err) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-            res.write('Hubo un error al almacenar el usuario.');
-            res.end();
-        } else {
-            users.find({}, function(err, users) {
-                if(err) {
-                    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-                    res.write('Hubo un error al acceder a la lista de usuarios.');
-                    res.end();
-                } else {
-//                    res.render('userslist', {
-//                        'userslist' : users
-//                    });
-                    res.redirect('/userslist');
-                }
-            });
+    addUser.save(
+        function(err, addUser) {
+            if (err) {
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                res.write('Hubo un error al almacenar el usuario.');
+                res.end();
+            } else {
+                users.find({}, function(err, users) {
+                    if(err) {
+                        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                        res.write('Hubo un error al acceder a la lista de usuarios.');
+                        res.end();
+                    } else {
+                        res.redirect('/userslist');
+                    }
+                });
+            }
         }
-    });
+    );
 });
 
 //Página que muestra los usuarios existentes
 router.get('/userslist', function(req, res) {
-    users.find({}, function(err, users) {
-        if(err) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-            res.write('Hubo un error al acceder a la lista de usuarios.');
-            res.end();
-        } else {
-            res.render('userslist', {
-                'userslist' : users
-            });
+    users.find(
+        {},
+        function(err, users) {
+            if(err) {
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                res.write('Hubo un error al acceder a la lista de usuarios.');
+                res.end();
+            } else {
+                res.render(
+                    'userslist', 
+                    { 'userslist' : users }
+                );
+            }
         }
-    });
+    );
 });
 
-//Con este método eleminamos un usuario según su ID
-router.get('/removeUser/:id', function(req, res) {
-    users.remove({_id: req.params.id}, function(err) {
-        if(err) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-            res.write('Hubo un error al intentar borrar el usuario.');
-            res.end();
-        } else {
-            res.redirect('/userslist');
+router.get('/editUser/:id/edit', function(req, res) {
+    users.findById(
+        req.params.id,
+        function(err, user) {
+            if(err) {
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                res.write('No se encontró el usuario.');
+                res.end();
+            } else {        
+                res.render(
+                    'edituser', 
+                    {'user' : user });
+            }
         }
-    });
+    );
+});
+
+router.post('/editUser/:id', function(req, res) {
+    users.update(
+        { _id : req.params.id },
+        { $set: {
+            'name' : req.param('name'),
+            'surname' : req.param('surname'),
+            'user' : req.param('user'),
+            'email' : req.param('email'),
+            'password' : req.param('password')
+        }}, 
+        function(err, user) {
+            if(err) {
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                res.write('Hubo un error al modificar los datos del usuario.');
+                res.end();
+            } else {
+                console.log(user);
+                res.redirect('/userslist');
+            }
+        }
+    );
+});
+
+//Con este método eliminamos un usuario según su ID
+router.get('/removeUser/:id', function(req, res) {
+    users.remove(
+        {_id: req.params.id},
+        function(err) {
+            if(err) {
+                res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+                res.write('Hubo un error al intentar borrar el usuario.');
+                res.end();
+            } else {
+                res.redirect('/userslist');
+            }
+        }
+    );
 });
 
 
